f7afe6d5f3b50cef3eed35205f6392271ebc02e8
Dmitry Evseev <ew.dim@mail.ru>
2023-06-22 01:02:32 +0300

    updated solve

--- 05-clang-introduction/guess_the_number/main.c
+++ 05-clang-introduction/guess_the_number/main.c
@@ -4,14 +4,16 @@
 #include <string.h>
 #include <stdbool.h>
 
+#define MAX_ATTEMPTS 10
+
 int get_random_number() {
 	// Use current time as seed for random generator
 	srand(time(NULL)); // NOLINT(cert-msc51-cpp)
-	return rand() % 100; // NOLINT(cert-msc50-cpp)
+	return rand() % 1000; // NOLINT(cert-msc50-cpp)
 }
 
 int get_input_number(bool *isLoop) { // NOLINT(misc-no-recursion)
-	char num1[256];
+	char num1[4];
 
 	printf("Введите число: ");
 	scanf("%s", num1);
@@ -19,25 +21,21 @@ int get_input_number(bool *isLoop) { // NOLINT(misc-no-recursion)
 		*isLoop = false;
 		return 0;
 	}
-	int _scanf = sscanf(num1, "%[0-9]d", num1);
-	if (_scanf != 1) {
-		printf("Number not found\n");
-		return get_input_number(isLoop);
-	}
 
 	return atoi(num1); // convert the string to int NOLINT(cert-err34-c)
 }
 
-bool is_equal_number(int input_number, int rand_number) {
+bool is_not_equal_number(int input_number, int rand_number, int number_attempt) {
 	if (input_number > rand_number) {
-		printf("Моё число меньше\n\n");
-		return false;
-	} else if (input_number < rand_number) {
-		printf("Моё число больше\n\n");
-		return false;
-	} else {
-		printf("\nВы угадали, загаданное число - %d", rand_number);
+		printf("(%d)Моё число меньше\n\n", number_attempt);
 		return true;
+	} else if (input_number < rand_number) {
+		printf("(%d)Моё число больше\n\n", number_attempt);
+		return true;
+	} else {
+		printf("\nВы угадали, загаданное число - %d. Количество попыток - %d", rand_number,
+			   MAX_ATTEMPTS - number_attempt);
+		return false;
 	}
 }
 
@@ -45,14 +43,18 @@ int main() {
 	printf("Введите q если хотите выйти\n\n");
 	int r_number = get_random_number();
 	bool isLoop = true;
-	while (1) {
-		int number = get_input_number(&isLoop);
-		if (isLoop == false) {
+	int number;
+	int number_attempt = MAX_ATTEMPTS;
+
+	do {
+		if (--number_attempt == -1) {
+			printf("\nВы проиграли, количество попыток исчерпано. Загаданное число - %d", r_number);
 			return 0;
 		}
-		if (is_equal_number(number, r_number))
-			break;
-	}
+		number = get_input_number(&isLoop);
+		if (isLoop == false)
+			return 0;
+	} while (is_not_equal_number(number, r_number, number_attempt));
 
 	return 0;
 }
